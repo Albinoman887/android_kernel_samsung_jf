5c400fa0aa52e2e210313ecf91e6b620ce325dcf
Matt Filetto <matt.filetto@gmail.com>
2013-05-15 22:40:18 -0700

    Start preparing for CPU OC
    Change-Id: I254d863b9bc192379c5cff114fe094f886d4acd5

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -48,6 +48,9 @@
 #define PRI_SRC_SEL_HFPLL_DIV2	2
 
 #define SECCLKAGD		BIT(4)
+
+#define FREQ_TABLE_SIZE		35
+
 #ifdef CONFIG_SEC_DEBUG_SUBSYS
 int boost_uv;
 int speed_bin;
@@ -985,7 +988,7 @@ void acpuclk_set_vdd(unsigned int khz, int vdd_uv) {
 #endif	/* CONFIG_CPU_VOTALGE_TABLE */
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 extern int console_batt_stat;
 static void __init cpufreq_table_init(void)
 {
